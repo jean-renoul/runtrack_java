37f4e998d09cf15910964c2d598779fc96916438
jean-renoul <jean.renoul@laplateforme.io>
2024-06-10 13:15:42 +0200

    job04 fini

---
+++ jour01/job04/DeclarationVariable.java
@@ -0,0 +1,9 @@
+package job04;
+
+public class DeclarationVariable {
+    public static void main(String args[]){
+        int var1; int var2; int var3;
+        var1 = 0; var2 = 0; var3 = 0;
+        System.out.println(var1 + "" + var2 + "" + var3);
+    }
+}
