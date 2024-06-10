735a2974bb2a0e36309c65039a6fdaf4a5db5ae0
jean-renoul <jean.renoul@laplateforme.io>
2024-06-10 12:00:01 +0200

    job02 et 03 fait

---
+++ jour01/job02/helloworld.java
@@ -0,0 +1,7 @@
+package job02;
+
+class HelloWorld{
+	public static void main(String args[]){
+		System.out.println("Hello world!");
+		}
+}
---
+++ jour01/job03/HelloWorld.java
@@ -0,0 +1,10 @@
+package job03;
+// commentaire sur une ligne
+public class HelloWorld {
+    public static void main(String args[]){
+		System.out.println("Hello world!");
+		}
+}
+/* Commentaire sur plusieurs lignes
+Commentaire sur plusieurs lignes
+Commentaire sur plusieurs lignes */
