bc230f48b99218f8dc9472a108f991f7af6aae7e
jean-renoul <jean.renoul@laplateforme.io>
2024-06-13 10:24:25 +0200

    job01 fini

---
+++ jour04/job01/MyThread.java
@@ -0,0 +1,11 @@
+package job01;
+
+
+public class MyThread extends Thread{
+
+    @Override
+    public void run() {
+        System.out.println("Mon thread est en cours");
+    }
+
+}
---
+++ jour04/job01/ThreadProject.java
@@ -0,0 +1,9 @@
+package job01;
+
+public class ThreadProject {
+    public static void main(String[] args) {
+        MyThread thread = new MyThread();
+        thread.start();
+    }
+
+}
